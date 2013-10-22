afc58c3283c92832dde6639ee6431007c28a5941
Xaree Lee <xareelee@gmail.com>
2013-10-22 14:28:36 +0800

    Update XIntrospect

--- XSpect/XIntrospect/XIntrospectCore.h
+++ XSpect/XIntrospect/XIntrospectCore.h
@@ -7,11 +7,20 @@
 /** A block type which will be synthesized by IntrospectionBlock. */
 typedef void (^Matryoshka)();
 
-/** A block type which decides how to synthesize AdviceBlock(s) and which one is returned. */
-// 3 choices: nil, innerMatryoshka, or a new Matryoshka containing an innerMatryoshka or not.
+/** 
+ A block type which decides how to synthesize Matryoshka(s) and which one is returned.
+ 
+ There are four kinds of Matryoshka which an IntrospectBlock can return:
+ 
+ 1. nil
+ 2. just the innerMatryoshka
+ 3. a new Matryoshka with an innerMatryoshka
+ 4. a new Matryoshka without an innerMatryoshka
+ 
+ */
 typedef Matryoshka (^IntrospectBlock)(Matryoshka innerMatryoshka);
 
-/** This function will compose an AdviceBlock according to the serial of IntrospectionBlock(s). It should be terminated with nil. */
+/** This function will assemble a Matryoshka according to the IntrospectionBlock(s) in the list. It should be terminated with nil. */
 Matryoshka assembleMatryoshka(IntrospectBlock introspection, ... )NS_REQUIRES_NIL_TERMINATION;
 
 
--- XSpect/XIntrospect/XIntrospectExtension.h
+++ XSpect/XIntrospect/XIntrospectExtension.h
@@ -7,10 +7,52 @@
 #import "XIntrospectCore.h"
 
 
-#define Introspect Matryoshka matryoshka = assembleMatryoshka(
-#define MainTask     ^ Matryoshka(Matryoshka innerMatryoshka){ \
-return ^(){
+/**
+ To define a IntrospectBlock easily, you can use the following macros.
+ */
+#define DescribeIntrospection ^Matryoshka(Matryoshka innerMatryoshka){ return ^(){
 
-#define EndIntrospection }; \
-},nil);     \
+#define EndDescribeIntrospection };};
+
+#define ContinueNextIntrospection if (innerMatryoshka) {innerMatryoshka();}
+
+
+
+/**
+ 
+ To use IntrospectBlock(s) with those macros, you should put the IntrospectBlock(s) in one of the following format.
+ 
+ 1. Just using defined IntrospectBlock(s):
+	
+         Introspect
+         // List IntrospectBlock(s) here. Every IntrospectBlock(s) should end with a comma (,).
+         // ...
+ 
+         EndIntrospect
+ 
+ 2. Using defined IntrospectBlock(s) and a block to do addtional code:
+ 
+		 Introspect
+		 // List IntrospectBlock(s) here. Every IntrospectBlock(s) should end with a comma (,).
+		 // ...
+		 
+		 MainTask
+		 // Add code here. This is inside a Matryoshka block.
+ 
+		 EndIntrospection
+
+ 
+ */
+#define Introspect Matryoshka matryoshka = assembleMatryoshka(
+
+#define MainTask DescribeIntrospection
+
+#define FurtherIntrospect };},
+
+#define EndIntrospectio1 nil);  \
 if (matryoshka) {matryoshka();}
+
+#define EndIntrospection FurtherIntrospect EndIntrospectio1	// Combine FurtherIntrospect and EndIntrospectio1
+
+
+
